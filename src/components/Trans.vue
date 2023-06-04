82405961c080b17d605defe33538af877fac3f58
daihaorui <Dai_Haorui@163.com>
2023-06-04 16:51:51 +0800

    Inital commit

---
+++ src/components/Trans.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-upload
+    class="upload-demo"
+    drag="true"
+    action="http://127.0.0.1:8002/translate/trans"
+    method="POST"
+    multiple="false"
+    accept=".csv"
+    show-file-list
+    name="trans"
+  >
+    <el-icon class="el-icon--upload"><upload-filled /></el-icon>
+    <div class="el-upload__text">
+      Drop file here or <em>click to upload</em>
+    </div>
+    <template #tip>
+      <div class="el-upload__tip">
+        jpg/png files with a size less than 500kb
+      </div>
+    </template>
+  </el-upload>
+</template>
+
+<script setup lang="ts">
+import { UploadFilled } from '@element-plus/icons-vue'
+</script>
