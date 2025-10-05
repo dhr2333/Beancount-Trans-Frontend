2ede11ef99ddd87b72e5430023545669ab2b9c14
daihaorui <Dai_Haorui@163.com>
2025-10-05 14:55:48 +0800

    Add ElButtonGroup component and refactor MappingManagement layout for analytics

--- src/components.d.ts
+++ src/components.d.ts
@@ -22,6 +22,7 @@ declare module '@vue/runtime-core' {
     ElBreadcrumb: typeof import('element-plus/es')['ElBreadcrumb']
     ElBreadcrumbItem: typeof import('element-plus/es')['ElBreadcrumbItem']
     ElButton: typeof import('element-plus/es')['ElButton']
+    ElButtonGroup: typeof import('element-plus/es')['ElButtonGroup']
     ElCard: typeof import('element-plus/es')['ElCard']
     ElCascader: typeof import('element-plus/es')['ElCascader']
     ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
